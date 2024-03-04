3d10859d1a121909aa6d21aad4c223eb4deba614
John Salama <johnotaku100@gmail.com>
2024-03-04 15:45:17 +0200

    version 4.0.0

--- Public/connectAndDis.js
+++ Public/connectAndDis.js
@@ -1,8 +1,12 @@
 const connectBtn = document.querySelector('#connectBtn');
 const disconnectBtn = document.querySelector('#disconnectBtn');
+const videoFrame = document.querySelector('.live-video');
+const container = document.querySelector('.video-container');
+
+const videoSrc =
+  'https://www.youtube.com/embed/MtN1YnoL46Q?si=HgGGWkikSHOk3JHN';
 
 const port_list = document.querySelector('#port-list');
-const passwordElem = document.querySelector('#paswordInput');
 connectBtn.addEventListener('click', async () => {
   const port = port_list.value;
   connectPort(port);
@@ -10,3 +14,8 @@ connectBtn.addEventListener('click', async () => {
 disconnectBtn.addEventListener('click', () => {
   disconnect();
 });
+
+if (!videoSrc.trim()) {
+  container.innerHTML = '<p>Please connect to MFR network.</p>';
+  container.style.border = '5px solid red';
+}
